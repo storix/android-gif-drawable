3ea9b52342849de18513a7632c5ad90a2627812f
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2017-03-05 20:36:52 +0100

    CI config refactor

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -2,16 +2,5 @@
 
 set -e
 
-OS=linux
-if [[ `uname` == 'Darwin' ]]; then
-    OS=darwin
-fi
-
-ANDROID_NDK_VERSION=r14
-
-cd
-wget -q https://dl.google.com/android/repository/android-ndk-${ANDROID_NDK_VERSION}-${OS}-x86_64.zip
-unzip -q android-ndk-${ANDROID_NDK_VERSION}-${OS}-x86_64.zip
-rm -rf ${ANDROID_NDK_HOME}
-mv android-ndk-${ANDROID_NDK_VERSION} ${ANDROID_NDK_HOME}
-cat ${ANDROID_NDK_HOME}/source.properties
+${ANDROID_HOME}/tools/bin/sdkmanager --update
+${ANDROID_HOME}/tools/bin/sdkmanager ndk-bundle
