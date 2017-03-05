faca8317f59cbe0529e2c17a59f34af20d992098
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2017-03-05 21:45:54 +0100

    CI config refactor

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -3,7 +3,7 @@
 set -e
 
 rm -rf ${ANDROID_NDK_HOME}
-${ANDROID_HOME}/tools/android update sdk --no-ui --filter tools
+yes | ${ANDROID_HOME}/tools/android update sdk --no-ui --filter tools
 ${ANDROID_HOME}/tools/bin/sdkmanager --update
 ${ANDROID_HOME}/tools/bin/sdkmanager ndk-bundle
 ln -s ${ANDROID_HOME}/ndk-bundle ${ANDROID_NDK_HOME}
